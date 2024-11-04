4efe5136ef299bb9f5f0ab8130f33ce4b1020fa1
Lyonko06 <109053235+Lyonko06@users.noreply.github.com>
2024-11-04 10:33:05 -0500

    Update economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "PERU TOP 1 EN PAISES POBRES",
+"autor" => "AY MI CERRU",
+"resumen" => "MIAU MIAU",
 ];
 ?>
