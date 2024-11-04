a2a62c94b86403f353272905e2a80addcb4f29a3
Lyonko06 <109053235+Lyonko06@users.noreply.github.com>
2024-11-04 10:20:47 -0500

    Update video.php

--- api/secciones/video.php
+++ api/secciones/video.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $video = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "candy perreo",
+"autor" => "PLAN B",
+"resumen" => "Canciones Antiguas del Regueaton",
 ];
 ?>
