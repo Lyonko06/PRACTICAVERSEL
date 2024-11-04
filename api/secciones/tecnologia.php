52a7a45f3c691d38a790a4fc88be587ec2a8bfa4
Lyonko06 <109053235+Lyonko06@users.noreply.github.com>
2024-11-04 10:48:13 -0500

    Update tecnologia.php

--- api/secciones/tecnologia.php
+++ api/secciones/tecnologia.php
@@ -4,8 +4,9 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $tecnologia = [
-"titulo" => "JAVA NO SIRVE EN LA ACTUALIDAD",
+"titulo" => "JAVA EN LA ACTUALIDAD",
 "autor" => "YO",
-"resumen" => "NO USO POR ESO NO ME SIRVE",
+"resumen" => "si habalamos de java hablamos de uno de los lenguajes de programacion mas populares de la actualidad tanto asi que su uso masivo en empresas peraunas a aumentado la demandad de enseñar este tipo de lenguaje ya que genra mas trabjo
+",
 ];
 ?>
