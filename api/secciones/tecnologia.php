203be0384828b50cd2f81d0d0a1194a340c4e15d
Lyonko06 <109053235+Lyonko06@users.noreply.github.com>
2024-11-04 10:23:11 -0500

    Update tecnologia.php

--- api/secciones/tecnologia.php
+++ api/secciones/tecnologia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $tecnologia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "JAVA NO SIRVE EN LA ACTUALIDAD",
+"autor" => "YO",
+"resumen" => "NO USO POR ESO NO ME SIRVE",
 ];
 ?>
